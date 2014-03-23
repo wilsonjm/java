235dc812040fe6e7c9794ca2961da7fd07f58f9f
jason <wilsonjm@g.cofc.edu>
2014-03-23 13:35:53 -0400

    eddited top line

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.lighttable/clike "0.0.1"
+(defproject com.lighttable/java "0.0.1"
  :description "Java language plugin for Light Table"
  :dependencies [[org.clojure/clojure "1.5.1"]])
 
