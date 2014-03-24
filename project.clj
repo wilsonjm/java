97efa35c48aa6516aa296ffa9968a947d8ca783a
jason <wilsonjm@g.cofc.edu>
2014-03-23 20:32:06 -0400

    bumped version num

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.lighttable/java "0.0.1"
+(defproject com.lighttable/java "0.0.3"
  :description "Java language plugin for Light Table"
  :dependencies [[org.clojure/clojure "1.5.1"]])
 
